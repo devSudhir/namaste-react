5ede2a8fefb38d80462d75e9ebb5f363c1d00003
devSudhir <sudhirsamantaray33@gmail.com>
2025-09-23 00:00:06 +0530

    episode 2 namaste react

--- Episode-1/app.js
+++ Episode-1/app.js
@@ -1,17 +1,12 @@
-// const h1 = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React"
-// );
-
-// console.log(h1);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "main-heading" }, "Main Heading"),
-    React.createElement("h2", { id: "sub-heading" }, "sub heading"),
+    React.createElement("h1", { id: "main-heading" }, "Main Heading1"),
+    React.createElement("h2", { id: "sub-heading" }, "sub heading1"),
   ])
 );
 
