0744ba48439e03d5ea0c4a6216af2b1fb8a2867c
devSudhir <sudhirsamantaray33@gmail.com>
2025-09-23 20:34:43 +0530

    Episode-3: Namaste react

--- Episode-1/app.js
+++ Episode-1/app.js
@@ -1,14 +1,37 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "main-heading" }, "Main Heading1"),
+//     React.createElement("h2", { id: "sub-heading" }, "sub heading1"),
+//   ])
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "main-heading" }, "Main Heading1"),
-    React.createElement("h2", { id: "sub-heading" }, "sub heading1"),
-  ])
-);
+// const h1 = React.createElement("h1", { id: "heading" }, "Namaste React 🚀");
+
+// const jsxHeading = <h1 id="heading">Namaste JSX</h1>;
+
+const Title = () => <span>Hello browser</span>;
+
+const HeaderComponent = () => {
+  console.log("react funcitonal component");
+  return (
+    <>
+      <Title />
+      <h1>Hello</h1>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeaderComponent />);
